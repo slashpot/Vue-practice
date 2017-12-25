17c778808587003186da55eca4db7eaff0190125
Eric Lin Shu Chin <eric.lin88@titansoft.com.sg>
2017-12-25 21:41:45 +0800

    add starbase

--- HelloWorld.js
+++ HelloWorld.js
@@ -2,6 +2,7 @@ var Startup = /** @class */ (function () {
     function Startup() {
     }
     Startup.main = function () {
+        console.log("hi");
         return 0;
     };
     return Startup;
--- HelloWorld.ts
+++ HelloWorld.ts
@@ -1,6 +1,6 @@
 class Startup {
     public static main(): number {
-         
+        console.log("hi"); 
         return 0;
     }
 }
