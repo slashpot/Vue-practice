69eea66cceefcbae48e6d82c3096578ead860206
Eric Lin Shu Chin <eric.lin88@titansoft.com.sg>
2017-12-25 22:16:05 +0800

    add charactor component

--- applications/starbase/src/App.vue
+++ applications/starbase/src/App.vue
@@ -1,16 +1,25 @@
 <template>
     <div id="app">
         <h3>{{title}}</h3>
+        <Charactor />
     </div>
 </template>
 
 <script>
+import Charactor from "./components/Character";
+
 export default {
   name: "app",
   data() {
     return {
       title: "Generate Your Team"
     };
+  },
+  methods: {
+      
+  },
+  components: {
+      Charactor
   }
 };
 </script>
---
+++ applications/starbase/src/components/Character.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      {{Character}}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      Character: {}
+    };
+  },
+  methods: {
+    fetchCharacter() {
+      fetch("https://swapi.co/api/people/1", {
+        method: "GET"
+      })
+        .then(response => response.json())
+        .then(data => (this.Character = data));
+    }
+  },
+  created() {
+      this.fetchCharacter();
+  }
+};
+</script>
